414df999b8388020279b3f07800d860c6b651448
shresree <124169954+shresree@users.noreply.github.com>
2023-11-06 08:27:38 -0500

    Add files via upload

---
+++ hw7-2021-main/js/video.js
@@ -0,0 +1,25 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+window.addEventListener("load", function() {
+    
+    video = document.querySelector("#play"); 
+
+    
+    if (video) {
+        
+        video.autoplay = false;
+        
+      
+        video.loop = false;
+    } else {
+        console.error("Video element not found");
+    }
+});
+ document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+ });
+
